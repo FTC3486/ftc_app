7a348ca1d5dfb90265749bf0da38bf39f4e10847
mdttrump97 <mdttrump97@bellsouth.net>
2017-03-06 17:01:29 -0600

    Fixes GyroAutoDriver to work with drivetrain and modifies the gyro turn function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Extension/GyroAutoDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Extension/GyroAutoDriver.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.Extension;
 
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
 /**
@@ -11,18 +10,17 @@ import com.qualcomm.robotcore.util.Range;
  */
 
 public class GyroAutoDriver {
-    public DcMotor Left1 = null;
-    public DcMotor Left2 = null;
-    public DcMotor Right1 = null;
-    public DcMotor Right2 = null;
-
+    private LinearOpMode opMode;
     private ModernRoboticsI2cGyro gyroSensor;  //ModernRoboticsI2cGyro allows us to .getIntegratedZValue()
+    private Drivetrain drivetrain;
 
-    public GyroAutoDriver(LinearOpMode opMode, String gyroSensorName)
+    public GyroAutoDriver(LinearOpMode inputOpMode, String gyroSensorName, Drivetrain inputDrivetrain)
     {
+        opMode = inputOpMode;
         gyroSensor = (ModernRoboticsI2cGyro) opMode.hardwareMap.gyroSensor.get(gyroSensorName);
         gyroSensor.calibrate();
         gyroSensor.resetZAxisIntegrator();
+        drivetrain = inputDrivetrain;
     }
 
     public void calibrate()
@@ -39,9 +37,11 @@ public class GyroAutoDriver {
 
         double target = gyroSensor.getIntegratedZValue();  //Starting direction
         double zAccumulated;
-        double startPosition = Left2.getCurrentPosition();  //Starting position
+        double startPositionLeft = drivetrain.getLeftEncoderCount();
+        double startPositionRight = drivetrain.getRightEncoderCount();
 
-        while (Left2.getCurrentPosition() < targetPosition + startPosition) {  //While we have not passed out intended distance
+        while (drivetrain.getLeftEncoderCount() < targetPosition + startPositionLeft
+                && drivetrain.getRightEncoderCount() < targetPosition + startPositionRight && opMode.opModeIsActive()) {  //While we have not passed out intended distance
             zAccumulated = gyroSensor.getIntegratedZValue();  //Current direction
 
             leftSpeed = power + (zAccumulated - target) / 20;  //Calculate speed for each side
@@ -50,16 +50,9 @@ public class GyroAutoDriver {
             leftSpeed = Range.clip(leftSpeed, -1, 1);
             rightSpeed = Range.clip(rightSpeed, -1, 1);
 
-            Left1.setPower(leftSpeed);
-            Left2.setPower(leftSpeed);
-            Right1.setPower(rightSpeed);
-            Right2.setPower(rightSpeed);
-
-            Left1.setPower(0);
-            Left2.setPower(0);//Stop the motors
-            Right1.setPower(0);
-            Right2.setPower(0);
+            drivetrain.setPowers(leftSpeed, rightSpeed);
         }
+        drivetrain.setPowers(0.0, 0.0);
     }
 
     public void driveStraightBackwards(int targetPosition, double power) {
@@ -68,11 +61,11 @@ public class GyroAutoDriver {
 
         double target = gyroSensor.getIntegratedZValue();  //Starting direction
         double zAccumulated;
-        double startPositionLeft = Left2.getCurrentPosition();//Starting position
-        double startPositionRight = Right2.getCurrentPosition();
+        double startPositionLeft = drivetrain.getLeftEncoderCount();//Starting position
+        double startPositionRight = drivetrain.getRightEncoderCount();
 
-        while (Left2.getCurrentPosition() > targetPosition + startPositionLeft &&
-                Right2.getCurrentPosition()> targetPosition + startPositionRight)
+        while (drivetrain.getLeftEncoderCount() > targetPosition + startPositionLeft &&
+               drivetrain.getRightEncoderCount() > targetPosition + startPositionRight && opMode.opModeIsActive())
         {  //While we have not passed out intended distance
             zAccumulated = gyroSensor.getIntegratedZValue();  //Current direction
 
@@ -82,51 +75,19 @@ public class GyroAutoDriver {
             leftSpeed = Range.clip(leftSpeed, -1, 1);
             rightSpeed = Range.clip(rightSpeed, -1, 1);
 
-            Left1.setPower(leftSpeed);
-            Left2.setPower(leftSpeed);
-            Right1.setPower(rightSpeed);
-            Right2.setPower(rightSpeed);
+            drivetrain.setPowers(leftSpeed, rightSpeed);
         }
-
-        Right1.setPower(0);
-        Right2.setPower(0);
-        Left1.setPower(0);
-        Left2.setPower(0);//Stop the motors
-    }
-
-    //This function turns a number of degrees compared to where the robot is. Positive numbers trn left.
-    public void turn(int target) throws InterruptedException {
-        turnAbsolute(target + gyroSensor.getIntegratedZValue());
+        drivetrain.setPowers(0.0, 0.0);
     }
 
     //This function turns a number of degrees compared to where the robot was when the program started. Positive numbers trn left.
-    public void turnAbsolute(int target) {
-        double zAccumulated;
-        zAccumulated = gyroSensor.getIntegratedZValue();  //Set variables to gyroSensor readings
-        double turnSpeed = 0.15;
+    public void turn(int target, double power) {
+        gyroSensor.resetZAxisIntegrator();
 
-        while (Math.abs(zAccumulated - target) > 3) {  //Continue while the robot direction is further than three degrees from the target
-            if (zAccumulated > target) {  //if gyroSensor is positive, we will turn right
-                Left1.setPower(turnSpeed);
-                Left2.setPower(turnSpeed);
-                Right1.setPower(-turnSpeed);
-                Right2.setPower(-turnSpeed);
-            }
+        drivetrain.setPowers(power, -power);
 
-            if (zAccumulated < target) {  //if gyroSensor is positive, we will turn left
-                Left1.setPower(-turnSpeed);
-                Left2.setPower(-turnSpeed);
-                Right1.setPower(turnSpeed);
-                Right2.setPower(turnSpeed);
-            }
+        while (gyroSensor.getHeading() < target && opMode.opModeIsActive()) {}
 
-            zAccumulated = gyroSensor.getIntegratedZValue();  //Set variables to gyroSensor readings
-
-        }
-
-        Left1.setPower(0);
-        Left2.setPower(0);
-        Right1.setPower(0);
-        Right2.setPower(0);
+        drivetrain.setPowers(0.0, 0.0);
     }
 }
