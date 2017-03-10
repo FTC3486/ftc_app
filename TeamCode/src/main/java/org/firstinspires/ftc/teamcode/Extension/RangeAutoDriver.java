9d983a92ac02ba5bdc2b7764441e00123271808d
mdttrump97 <mdttrump97@bellsouth.net>
2017-03-10 15:15:17 -0600

    Modified RangeAutoDriver to get sensors from hardware configuration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Extension/RangeAutoDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Extension/RangeAutoDriver.java
@@ -1,10 +1,6 @@
 package org.firstinspires.ftc.teamcode.Extension;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.I2cAddr;
-import com.qualcomm.robotcore.hardware.I2cDevice;
-import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
-import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
 import com.qualcomm.robotcore.util.Range;
 
 /**
@@ -13,52 +9,23 @@ import com.qualcomm.robotcore.util.Range;
 
 public class RangeAutoDriver {
     private LinearOpMode opMode;
-    private Drivetrain drivetrain;
-    private I2cDevice sideSensor;
-    private I2cDeviceSynch sideSensorReader;
-    private I2cDevice frontSensor;
-    private I2cDeviceSynch frontSensorReader;
+    private HardwareConfiguration hw;
 
-
-    public RangeAutoDriver(LinearOpMode opMode, Drivetrain drivetrain, String frontSensor, String sideSensor)
+    public RangeAutoDriver(LinearOpMode opMode, HardwareConfiguration hw)
     {
         this.opMode = opMode;
-        this.drivetrain = drivetrain;
-
-        this.frontSensor = opMode.hardwareMap.i2cDevice.get(frontSensor);
-        this.frontSensorReader = new I2cDeviceSynchImpl(this.frontSensor, I2cAddr.create8bit(0x28), false);
-        this.frontSensorReader.engage();
-
-        this.sideSensor = opMode.hardwareMap.i2cDevice.get(sideSensor);
-        this.sideSensorReader = new I2cDeviceSynchImpl(this.sideSensor, I2cAddr.create8bit(0x2a), false);
-        this.sideSensorReader.engage();
-    }
-
-    public double getSideUltrasonicRange()
-    {
-        //Read 2 bytes at a time, starting at 0x04
-        byte[] sideSensorCache = sideSensorReader.read(0x04, 2);
-        //sideSensorCache[0] holds Ultrasonic Values; & 0xFF converts the values to be in the range 0-255;
-        return sideSensorCache[0] & 0xFF;
-    }
-
-    public double getFrontUltrasonicRange()
-    {
-        //Read 2 bytes at a time, starting at 0x04
-        byte[] frontSensorCache = frontSensorReader.read(0x04, 2);
-        //frontSensorCache[0] holds Ultrasonic Values; & 0xFF converts the values to be in the range 0-255;
-        return frontSensorCache[0] & 0xFF;
+        this.hw = hw;
     }
 
     public void wallFollowForwards(double power, double RangeCm, int encodercounts){
-        double startPositionLeft = drivetrain.getLeftEncoderCount();//Starting position
-        double startPositionRight = drivetrain.getRightEncoderCount();
+        double startPositionLeft = hw.drivetrain.getLeftEncoderCount();//Starting position
+        double startPositionRight = hw.drivetrain.getRightEncoderCount();
 
-        while(drivetrain.getLeftEncoderCount() < encodercounts + startPositionLeft && drivetrain.getRightEncoderCount()< encodercounts + startPositionRight && opMode.opModeIsActive())  {
+        while(hw.drivetrain.getLeftEncoderCount() < encodercounts + startPositionLeft && hw.drivetrain.getRightEncoderCount()< encodercounts + startPositionRight && opMode.opModeIsActive())  {
             double leftSpeed = power;
             double rightSpeed = power;
 
-            double sideUltrasonicRange = this.getSideUltrasonicRange();
+            double sideUltrasonicRange = hw.sideRangeSensor.getUltrasonicRange();
 
             if(sideUltrasonicRange > RangeCm)
             {
@@ -72,19 +39,19 @@ public class RangeAutoDriver {
             leftSpeed = Range.clip(leftSpeed, -1, 1);
             rightSpeed = Range.clip(rightSpeed, -1, 1);
 
-            drivetrain.setPowers(leftSpeed, rightSpeed);
+            hw.drivetrain.setPowers(leftSpeed, rightSpeed);
         }
     }
 
     public void wallFollowBackwards(double power, double RangeCm, int encodercounts){
-        double startPositionLeft = drivetrain.getLeftEncoderCount();//Starting position
-        double startPositionRight = drivetrain.getRightEncoderCount();
+        double startPositionLeft = hw.drivetrain.getLeftEncoderCount();//Starting position
+        double startPositionRight = hw.drivetrain.getRightEncoderCount();
 
-        while(drivetrain.getLeftEncoderCount() > encodercounts + startPositionLeft && drivetrain.getRightEncoderCount() > encodercounts + startPositionRight && opMode.opModeIsActive())  {
+        while(hw.drivetrain.getLeftEncoderCount() > encodercounts + startPositionLeft && hw.drivetrain.getRightEncoderCount() > encodercounts + startPositionRight && opMode.opModeIsActive())  {
             double leftSpeed = power;
             double rightSpeed = power;
 
-            double sideUltrasonicRange = this.getSideUltrasonicRange();
+            double sideUltrasonicRange = hw.sideRangeSensor.getUltrasonicRange();
             if(sideUltrasonicRange > RangeCm)
             {
                 leftSpeed = power - 0.005;
@@ -97,7 +64,7 @@ public class RangeAutoDriver {
             leftSpeed = Range.clip(leftSpeed, -1, 1);
             rightSpeed = Range.clip(rightSpeed, -1, 1);
 
-            drivetrain.setPowers(leftSpeed, rightSpeed);
+            hw.drivetrain.setPowers(leftSpeed, rightSpeed);
         }
     }
 }
