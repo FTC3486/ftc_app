0b7b86dd8b6f62231b097e8f6b15483f3e62e8a0
jefflanum <jefflanum@yahoo.com>
2017-03-09 22:06:25 -0600

    Adds forwards and backwards wall follow methods using the side range sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Extension/RangeAutoDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Extension/RangeAutoDriver.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.I2cDevice;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by Matthew on 3/8/2017.
@@ -47,5 +48,53 @@ public class RangeAutoDriver {
         byte[] frontSensorCache = frontSensorReader.read(0x04, 2);
         //frontSensorCache[0] holds Ultrasonic Values; & 0xFF converts the values to be in the range 0-255;
         return frontSensorCache[0] & 0xFF;
+    }
+
+    public void wallFollowForwards(double power, double RangeCm, int encodercounts){
+        double startPositionLeft = drivetrain.getLeftEncoderCount();//Starting position
+        double startPositionRight = drivetrain.getRightEncoderCount();
+
+        while(drivetrain.getLeftEncoderCount() < encodercounts + startPositionLeft && drivetrain.getRightEncoderCount()< encodercounts + startPositionRight && opMode.opModeIsActive())  {
+            double leftSpeed = power;
+            double rightSpeed = power;
+
+            if(this.getSideUltrasonicRange() > RangeCm)
+            {
+                rightSpeed = power - 0.005;
+            }
+            else if(this.getSideUltrasonicRange() < RangeCm)
+            {
+                leftSpeed = power - 0.005;
+            }
+
+            leftSpeed = Range.clip(leftSpeed, -1, 1);
+            rightSpeed = Range.clip(rightSpeed, -1, 1);
+
+            drivetrain.setPowers(leftSpeed, rightSpeed);
+        }
+    }
+
+    public void wallFollowBackwards(double power, double RangeCm, int encodercounts){
+        double startPositionLeft = drivetrain.getLeftEncoderCount();//Starting position
+        double startPositionRight = drivetrain.getRightEncoderCount();
+
+        while(drivetrain.getLeftEncoderCount() > encodercounts + startPositionLeft && drivetrain.getRightEncoderCount() > encodercounts + startPositionRight && opMode.opModeIsActive())  {
+            double leftSpeed = power;
+            double rightSpeed = power;
+
+            if(this.getSideUltrasonicRange() > RangeCm)
+            {
+                leftSpeed = power - 0.005;
+            }
+            else if(this.getSideUltrasonicRange() < RangeCm)
+            {
+                rightSpeed = power - 0.005;
+            }
+
+            leftSpeed = Range.clip(leftSpeed, -1, 1);
+            rightSpeed = Range.clip(rightSpeed, -1, 1);
+
+            drivetrain.setPowers(leftSpeed, rightSpeed);
+        }
     }
 }
