d5edac522dc5f49e9dd3a642f749fa3d247b7281
Jacob Mason <jacob@jacobmason.net>
2017-03-11 15:21:00 -0600

    Added convenience collections to Drivetrain and fixed small bug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Extension/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Extension/Drivetrain.java
@@ -3,15 +3,18 @@ package org.firstinspires.ftc.teamcode.Extension;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class Drivetrain {
     private double wheelDiameter;
     private double gearRatio;
     private int encoderCountsPerDriverGearRotation;
-    private LinkedList<DcMotor> leftMotors;
-    private LinkedList<DcMotor> rightMotors;
-    private LinkedList<DcMotor> leftMotorsWithEncoders;
-    private LinkedList<DcMotor> rightMotorsWithEncoders;
+    private List<DcMotor> allMotors;
+    private List<DcMotor> allMotorsWithEncoders;
+    private List<DcMotor> leftMotors;
+    private List<DcMotor> rightMotors;
+    private List<DcMotor> leftMotorsWithEncoders;
+    private List<DcMotor> rightMotorsWithEncoders;
     private double leftSpeed;
     private double rightSpeed;
 
@@ -19,23 +22,35 @@ public class Drivetrain {
         this.wheelDiameter = builder.wheelDiameter;
         this.gearRatio = builder.gearRatio;
         this.encoderCountsPerDriverGearRotation = builder.encoderCountsPerDriverGearRotation;
-        this.leftMotors = builder.leftMotors;
-        this.rightMotors = builder.rightMotors;
         this.leftMotorsWithEncoders = builder.leftMotorsWithEncoders;
         this.rightMotorsWithEncoders = builder.rightMotorsWithEncoders;
+        this.leftMotors = builder.leftMotors;
+        this.leftMotors.addAll(builder.leftMotorsWithEncoders);
+        this.rightMotors = builder.rightMotors;
+        this.rightMotors.addAll(builder.rightMotorsWithEncoders);
+        this.allMotorsWithEncoders = new LinkedList<>();
+        this.allMotorsWithEncoders.addAll(builder.leftMotorsWithEncoders);
+        this.allMotorsWithEncoders.addAll(builder.rightMotorsWithEncoders);
+        this.allMotors = new LinkedList<>();
+        this.allMotors.addAll(this.leftMotors);
+        this.allMotors.addAll(this.rightMotors);
+
+        for (DcMotor motor : allMotors) {
+            motor.setZeroPowerBehavior(builder.zeroPowerBehavior);
+        }
     }
 
     public static class Builder {
         private double wheelDiameter = 4.0;
         private double gearRatio = 1.19;
-        // 1120 is the number for the AndyMark motors. Tetrix Motors are 1440 PPR
+        // 1120 is the number for the AndyMark allMotors. Tetrix Motors are 1440 PPR
         private int encoderCountsPerDriverGearRotation = 1120;
-        private final LinkedList<DcMotor> leftMotors = new LinkedList<DcMotor>();
-        private final LinkedList<DcMotor> rightMotors = new LinkedList<DcMotor>();
-        private final LinkedList<DcMotor> leftMotorsWithEncoders =
-                new LinkedList<DcMotor>();
-        private final LinkedList<DcMotor> rightMotorsWithEncoders =
-                new LinkedList<DcMotor>();
+        private final List<DcMotor> leftMotors = new LinkedList<>();
+        private final List<DcMotor> rightMotors = new LinkedList<>();
+        private final List<DcMotor> leftMotorsWithEncoders = new LinkedList<>();
+        private final List<DcMotor> rightMotorsWithEncoders = new LinkedList<>();
+        private DcMotor.ZeroPowerBehavior zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE;
+
 
         public Builder setWheelDiameter(double wheelDiameter) {
             this.wheelDiameter = wheelDiameter;
@@ -49,6 +64,11 @@ public class Drivetrain {
 
         public Builder setEncoderCountsPerDriverGearRotation(int encoderCountsPerDriverGearRotation) {
             this.encoderCountsPerDriverGearRotation = encoderCountsPerDriverGearRotation;
+            return this;
+        }
+
+        public Builder setZeroPowerBehavior(DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
+            this.zeroPowerBehavior = zeroPowerBehavior;
             return this;
         }
 
@@ -77,43 +97,15 @@ public class Drivetrain {
         }
     }
 
-    protected LinkedList<DcMotor> getLeftMotorsWithEncoders() {
-        return leftMotorsWithEncoders;
-    }
-
-    protected LinkedList<DcMotor> getRightMotorsWithEncoders() {
-        return rightMotorsWithEncoders;
-    }
-
     public void haltDrive() {
-        for (DcMotor motor : leftMotorsWithEncoders) {
-            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            motor.setPower(0);
-        }
-
-        for (DcMotor motor : rightMotorsWithEncoders) {
-            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            motor.setPower(0);
-        }
-
-        for (DcMotor motor : leftMotors) {
-            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            motor.setPower(0);
-        }
-
-        for (DcMotor motor : rightMotors) {
-            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        for (DcMotor motor : allMotors) {
             motor.setPower(0);
         }
     }
 
     public void setMode(DcMotor.RunMode runMode) {
-        for (DcMotor leftMotor : leftMotorsWithEncoders) {
-            leftMotor.setMode(runMode);
-        }
-
-        for (DcMotor rightMotor : rightMotorsWithEncoders) {
-            rightMotor.setMode(runMode);
+        for (DcMotor motor : allMotorsWithEncoders) {
+            motor.setMode(runMode);
         }
     }
 
@@ -128,32 +120,17 @@ public class Drivetrain {
     }
 
     public boolean isBusy() {
-        for (DcMotor leftMotor : leftMotorsWithEncoders) {
-            if (leftMotor.isBusy()) {
+        for (DcMotor motor : allMotorsWithEncoders) {
+            if (motor.isBusy()) {
                 return true;
             }
         }
-
-        for (DcMotor rightMotor : rightMotorsWithEncoders) {
-            if (rightMotor.isBusy()) {
-                return true;
-            }
-        }
-        
         return false;
     }
 
     public void setPowers(double leftSpeed, double rightSpeed) {
         this.leftSpeed = leftSpeed;
         this.rightSpeed = rightSpeed;
-
-        for (DcMotor motor : leftMotorsWithEncoders) {
-            motor.setPower(leftSpeed);
-        }
-
-        for (DcMotor motor : rightMotorsWithEncoders) {
-            motor.setPower(rightSpeed);
-        }
 
         for (DcMotor motor : leftMotors) {
             motor.setPower(leftSpeed);
@@ -170,39 +147,41 @@ public class Drivetrain {
     }
 
     protected double getLeftEncoderCount() {
-        double sumValue = 0;
-
-        for (DcMotor leftMotorWithEncoder : leftMotorsWithEncoders) {
-            sumValue += leftMotorWithEncoder.getCurrentPosition();
+        if (leftMotorsWithEncoders.size() == 0) {
+            return 0;
         }
 
-        sumValue = sumValue / (leftMotorsWithEncoders.size());
+        double sumValue = 0;
+
+        for (DcMotor leftMotor : leftMotorsWithEncoders) {
+            sumValue += leftMotor.getCurrentPosition();
+        }
+
+        sumValue /= (leftMotorsWithEncoders.size());
         return sumValue;
     }
 
     protected double getRightEncoderCount() {
-        double sumValue = 0;
-
-        for (DcMotor rightMotorWithEncoder : rightMotorsWithEncoders) {
-            sumValue += rightMotorWithEncoder.getCurrentPosition();
+        if (rightMotorsWithEncoders.size() == 0) {
+            return 0;
         }
 
-        sumValue = sumValue / leftMotorsWithEncoders.size();
+        double sumValue = 0;
+
+        for (DcMotor rightMotor : rightMotorsWithEncoders) {
+            sumValue += rightMotor.getCurrentPosition();
+        }
+
+        sumValue /= rightMotorsWithEncoders.size();
         return sumValue;
     }
 
     public void resetMotorEncoders() {
-        for (DcMotor leftMotorWithEncoders : leftMotorsWithEncoders) {
-            leftMotorWithEncoders.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            leftMotorWithEncoders.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
-
-        for (DcMotor rightMotorWithEncoders : rightMotorsWithEncoders) {
-            rightMotorWithEncoders.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            rightMotorWithEncoders.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        for (DcMotor motor : allMotorsWithEncoders) {
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
     }
-
 
     @Override
     public String toString() {
